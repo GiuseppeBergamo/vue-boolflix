fb0a57d877f4b885479558a3f9636210b7d273c8
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-08-21 13:06:52 +0200

    added MovieSection component

---
+++ src/components/MovieSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <section id="movies">
+    <h2>Movies</h2>
+    <ul v-for="movie in movies" :key="movie.id">
+        <li>{{ movie.title }}</li>
+        <li>{{ movie.original_title }}</li>
+        <li>{{ movie.original_language }}</li>
+        <li>{{ movie.vote_average }}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'MovieSection', 
+    props: {
+        movies: Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
