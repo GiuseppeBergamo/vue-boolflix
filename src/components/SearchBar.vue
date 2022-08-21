e170eb649671d556ed17fa984872739a9d15e187
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-08-21 12:57:33 +0200

    added SearchBar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="text" :placeholder="placeholder" v-model="text" @keyup.enter="emitSearch">
+    <button type="button" @click="emitSearch"> {{ buttonText }} </button>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'SearchBar',
+data(){
+    return {
+        text: '',
+    }
+},
+props: {
+    placeholder: String,
+    buttonText: String,
+},
+methods: {
+    emitSearch(){
+        this.$emit('search', this.text)
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
