4a0a3e6a017b7d30a2f23fcf108437754c23e7bc
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-08-21 16:27:48 +0200

    added SeriesSection component

---
+++ src/components/SeriesSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <section id="movies">
+    <h2>TV Series</h2>
+    <ul v-for="serie in series" :key="serie.id">
+        <li>{{ serie.name }}</li>
+        <li>{{ serie.original_name }}</li>
+        <li>{{ serie.original_language }}</li>
+        <li>{{ serie.vote_average }}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'SeriesSection', 
+    props: {
+        series: Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
