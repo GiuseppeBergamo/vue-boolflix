8ef5f9d67ff221447f16111fee08f98131806572
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-08-22 15:43:45 +0200

    added hover event animation

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,17 +1,22 @@
 <template>
     <div class="col">
-        <img v-if="!card.poster_path" src="../assets/img/No-Image-Placeholder.svg.png" alt="no-image" class="img-fluid rounded h-100">
-        <img v-else :src="posterSrc" alt="" id="poster" class="img-fluid rounded h-100">
+        <div @mouseenter="hover = true" @mouseleave="hover = false" id="posters" class="position-relative text-center h-100">
+            <img v-if="!card.poster_path" src="../assets/img/No-Image-Placeholder.svg.png" alt="no-image" class="img-fluid rounded h-100">
+            <img v-else :src="posterSrc" alt="" id="poster" class="img-fluid rounded h-100">
 
-        <ul class="d-none">
-            <li>{{ card.name || card.title}}</li>
-            <li>{{ card.original_name || card.original_title }}</li>
-            <li>
-                <img v-if="card.original_language === 'it' || 'en'" :src="flagImageSrc" :alt="card.original_language" class="w-25">
-                <span v-else>{{ card.original_language }}</span>
-            </li>
-            <li>{{ voteNumber }}</li>
-        </ul>
+            <div id="info" :class="{ 'active' : hover }" class="position-absolute top-0 start-0 h-100 p-5 text-start bg-dark rounded">
+                <ul class="text-white p-0 m-0">
+                    <li class="mb-3"><span class="me-2"><b>Titolo:</b></span>{{ card.name || card.title}}</li>
+                    <li class="mb-3"><span class="me-2"><b>Titolo originale:</b></span>{{ card.original_name || card.original_title }}</li>
+                    <li class="mb-3">
+                        <span class="me-2"><b>Lingua:</b></span>
+                        <img v-if="card.original_language === 'it' || 'en'" :src="flagImageSrc" :alt="card.original_language" class="w-25">
+                        <span v-else>{{ card.original_language }}</span>
+                    </li>
+                    <li class="mb-3"><span class="me-2"><b>Voto:</b></span>{{ voteNumber }}</li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -20,6 +25,11 @@ export default {
     name: 'BaseCard',
     props: {
         card: Object,
+    },
+    data() {
+        return {
+            hover: false
+        }
     },
     computed: {
         flagImageSrc(){
@@ -31,10 +41,29 @@ export default {
         voteNumber(){
             return Math.ceil((this.card.vote_average) / 2)
         }
-    }
+    },
+    
+        
 }
 </script>
 
 <style scoped>
+#info{
+    display: none;
+    
+}
+
+#info.active{
+   display: block;
+   opacity: 0.50;
+   transition: opacity 0.7s ease 0s; 
+}
+
+
+#info.active:hover{
+    opacity: 1;
+    cursor: pointer;
+}
+
 
 </style>
